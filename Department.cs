baf996d1d4017d9ece9d5403c1738de18e83e16e
AlexID-0987 <a1980dominov@gmail.com>
2021-01-30 17:53:13 +0200

    Linq

---
+++ Department.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq3
+{
+    class Department
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public static IEnumerable<Department> GetDepart()
+        {
+            return new List<Department>
+            {
+
+            new Department(){ Id = 1, Country = "Ukraine", City = "Donetsk" },
+            new Department(){ Id = 2, Country = "Ukraine", City = "Kyiv" },
+            new Department(){ Id = 3, Country = "France", City = "Paris" },
+            new Department(){ Id = 4, Country = "Russia", City = "Moscow" }
+            };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Emp = Employee.GetEmployee();
+            var Dep = Department.GetDepart();
+            
+            var collec = Emp.Join(
+                          Dep,
+                          e => e.DepId,
+                          d => d.Id,
+                          (e, d) => new {
+                              id = d.Id,
+                              Country = d.Country,
+                              City = d.City,
+                              FirstName = e.FirstName,
+                              LastName = e.LastName,
+                              Age = e.Age
+
+                          }
+                      ).Where(x => x.Country == "Ukraine")
+                      .OrderBy(e=>e.FirstName);
+
+            collec.ToList().ForEach(collects => Console.WriteLine(
+                           $"{collects.id,-15} - " +
+                           $"{collects.Country,-15} - " +
+                           $"{collects.FirstName,-15} - " +
+                           $"{collects.LastName,-15}")
+                           );
+            Console.WriteLine();
+            //2
+            var coll2 = Emp.Where(Employee => Employee.Id > 0)
+                       .OrderBy(Employee => -Employee.Age);
+                       
+                         
+
+            foreach (var Employee in coll2)
+            {
+                Console.WriteLine($"{Employee.Id,-15}-"+$"{Employee.FirstName,-15}-" +
+                    $"{Employee.LastName,-15}-"+$"{Employee.Age,-15}");
+            }
+            Console.WriteLine();
+            
+            //3
+            var coll3 = from Employee in Emp
+                        group Employee by Employee.Age;
+            foreach (var group in coll3)
+            {
+                Console.WriteLine($"{group.Key}-{group.Count()}");
+            }
+        }
+    }
+}
