30eeeaa4f17273e18e6c637f65b8b95adee5af17
AlexID-0987 <a1980dom@gmail.com>
2021-04-29 22:19:10 +0300

    WPF ADO.NET

---
+++ Name_random_a/Name_random_a/MainViewMod.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Name_random_a
+{
+    class MainViewMod : INotifyPropertyChanged
+    {
+        private ObservableCollection<Student> students;
+        private Student selectedStudetn;
+        public ObservableCollection<Student> Students 
+        {
+            get => students;
+            set
+            {
+                students = value;
+                NotifyOfPropertyChanged();
+            }
+        } 
+        public Student SelectedStudent 
+        {
+            get => selectedStudetn;
+            set
+            {
+                selectedStudetn = value;
+                NotifyOfPropertyChanged();
+            }
+        }
+        public MainViewMod()
+        {
+            Students = new ObservableCollection<Student>();
+            InitCommands();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyOfPropertyChanged([CallerMemberName]string propertyName=" ")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void InitCommands()
+        {
+            AddStudentCommand = new NewCommand(x =>
+             {
+                 Students.Add(new Student
+                 {
+                     Name=RandomNameGenerator.NameGenerator.GenerateFirstName(RandomNameGenerator.Gender.Female),
+                     Lastname = RandomNameGenerator.NameGenerator.GenerateLastName()
+                 });
+             });
+            RemoweStudentCommand = new NewCommand(x =>
+             {
+                 Students.Remove(SelectedStudent);
+             });
+            ChangeCommand = new NewCommand(x =>
+            {
+                SelectedStudent.Color = "Blue";
+                
+            });
+            ChangeCollectCommand = new NewCommand(x =>
+              {
+                  Students = new ObservableCollection<Student>
+                  {
+                new Student{Name="Coco",Lastname="Klec"},
+                  };
+  
+              });
+        }
+        public ICommand AddStudentCommand { get; set; }
+        public ICommand RemoweStudentCommand { get; set; }
+        public ICommand ChangeCommand { get; set; }
+        public ICommand ChangeCollectCommand { get; set; }
+    }
+}
---
+++ Name_random_a/Name_random_a/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Name_random_a
+{
+    class Student : INotifyPropertyChanged
+    {
+        private string name;
+        private string lastname;
+        private string color = "Green";
+
+        public string Name
+        {
+            get => name; set
+            {
+                name = value;
+                NotifyOfPropertyChanged();
+            }
+
+        }
+        public string Lastname
+        { 
+            get => lastname; set
+            { 
+                  lastname = value;
+                  NotifyOfPropertyChanged();
+            }
+        }
+        public string Color 
+        { 
+            get => color; set
+            {
+                color = value;
+                NotifyOfPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyOfPropertyChanged([CallerMemberName]string propertyName="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); 
+        }
+
+        public override string ToString() => $"{Name} {Lastname}";
+        
+        
+    }
+}
