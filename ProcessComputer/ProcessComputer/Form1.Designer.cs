854f35e00ba375f29f2a6aa0e101ca681895279d
AlexID-0987 <a1980dom@gmail.com>
2021-06-07 20:38:29 +0300

    Thread C#

---
+++ ProcessComputer/ProcessComputer/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace ProcessComputer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ColumnHeader columnHeader4;
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Pryority";
+            columnHeader4.Width = 100;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(15, 85);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(647, 233);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Process Name";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Process Id";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Window Name";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Informatiom";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Salmon;
+            this.button1.Location = new System.Drawing.Point(47, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start Process";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.button2.Location = new System.Drawing.Point(212, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Kill";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(536, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button3.Location = new System.Drawing.Point(524, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 39);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Close";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button4.Location = new System.Drawing.Point(386, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(82, 39);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Update";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(11, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Filter Procees";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Mashine Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(680, 330);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "My Process";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+    }
+}
+
---
+++ ProcessComputer/ProcessComputer/Form1.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessComputer
+{
+    public partial class Form1 : Form
+    {
+        private List<Process> processes = null;
+        
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+        private void GetProc()
+        {
+            processes.Clear();
+            processes = Process.GetProcesses().ToList<Process>();
+        }
+
+        private void RefreshList()
+        {
+            listView1.Items.Clear();
+            foreach(Process p in processes)
+            {
+                PerformanceCounter pc = new PerformanceCounter();
+                pc.CategoryName = "Process";
+                pc.CounterName = "working set";
+                pc.InstanceName = p.ProcessName;
+                string[] row = new string[] { p.ProcessName.ToString(),
+                    p.Id.ToString(),p.MainWindowTitle.ToString(),p.BasePriority.ToString(),p.StartInfo.ToString()
+                   
+                };
+                listView1.Items.Add(new ListViewItem(row));
+                pc.Close();
+                pc.Dispose();
+            }
+            Text = "Process " + processes.Count.ToString();
+        }
+        private void RefreshList(List<Process> processes, string keyword)
+        {
+            listView1.Items.Clear();
+            foreach (Process p in processes)
+            {
+                PerformanceCounter pc = new PerformanceCounter();
+                pc.CategoryName = "Process";
+                pc.CounterName = "working set";
+                pc.InstanceName = p.ProcessName;
+                string[] row = new string[] { p.ProcessName.ToString(),
+                    p.Id.ToString(),p.MainWindowTitle.ToString(),p.BasePriority.ToString(),p.StartInfo.ToString()
+                    
+                };
+                listView1.Items.Add(new ListViewItem(row));
+                pc.Close();
+                pc.Dispose();
+            }
+            Text = "Process " + processes.Count.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = Interaction.InputBox("Name program", "Start Process");
+            try 
+            {
+                Process.Start(path);
+            }
+            catch (Exception) { }
+
+        }
+        private void KillProsess(Process process)
+        {
+            process.Kill();
+            process.WaitForExit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            try 
+            { 
+               if (listView1.SelectedItems[0]!=null)
+                {
+
+                    Process processToKill = processes.Where((x) => x.ProcessName ==
+                    listView1.SelectedItems[0].SubItems[0].Text).ToList()[0];
+                    KillProsess(processToKill);
+                    GetProc();
+                    RefreshList();
+
+
+
+
+
+                }
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            processes = new List<Process>();
+            GetProc();
+            RefreshList();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            GetProc();
+            RefreshList();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            GetProc();
+            List<Process> filter = processes.Where((x) => x.ProcessName.ToLower().
+              Contains(textBox1.Text.ToLower())).ToList<Process>();
+            RefreshList (filter, textBox1.Text);
+        }
+    }
+}
